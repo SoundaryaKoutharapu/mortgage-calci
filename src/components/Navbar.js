78fa933e250a867ed436d2432f379be86954c499
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-05-23 20:03:24 +0530

    MUI container

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const NavBar = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h5" component="div" sx={{ color: 'yellowgreen' }}>
-          Bank of React
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ color: 'yellowgreen' }}>
+            Bank of React
+          </Typography>
+        </Toolbar>
+      </Container>
     </AppBar>
   )
 }
