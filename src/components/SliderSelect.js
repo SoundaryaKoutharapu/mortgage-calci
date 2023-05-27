46f02aba7f10e56261314d3c653996d28b3bc4bb
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-05-27 12:59:34 +0530

    props destructing

--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -3,7 +3,12 @@ import SliderComponent from "./common/SliderComponent"
 const SliderSelect = () => {
   return (
     <> 
-    <SliderComponent defaultValue={150} min={100} max={200}/>
+    <SliderComponent defaultValue={150} 
+    min={100} 
+    max={200}
+    step ={5}
+    onChange={(e)=> console.log(e.target.value)}
+    />
     </>
   )
 }
--- src/components/common/SliderComponent.js
+++ src/components/common/SliderComponent.js
@@ -1,9 +1,24 @@
+import { Typography } from '@mui/material';
 import Slider from '@mui/material/Slider';
 
-const SliderComponent = ({defaultValue, min, max}) => {
+const SliderComponent = ({ defaultValue, min, max, step, onChange, value }) => {
 
   return (
-    <Slider defaultValue={defaultValue} min={min} max={max} aria-label="Default" valueLabelDisplay="auto" />
+    <>
+      <Typography variant='subtitle2'>Home Value</Typography>
+      <Typography variant='h6' sx={{mt:'5px'}}> $ 3000 </Typography>
+      <Slider defaultValue={defaultValue}
+        min={min}
+        max={max}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks
+        step={step}
+        onChange={onChange}
+        value={value}
+      />
+      <Typography></Typography>
+    </>
   )
 }
 export default SliderComponent
